16721b1ceb7f3738617901568206b6d985fc2b7d
Jordan <jr791633@dal.ca>
2021-03-30 15:56:33 -0300

    Added page with infographics and update to auth
    Added page with covid vaccination infographics from OurWorldData. This
    is based on the logged in user. Updated auth to better hide and show for
    logged in users.

--- assets/js/authUtil.js
+++ assets/js/authUtil.js
@@ -1,16 +1,39 @@
+// Logout the user from firebase authentication
 function logoutUser() {
     firebase.auth().signOut()
     window.location.href="index.html";
 }
 
+// Show all elements in the array of itemsToShow
 function showItems(itemsToShow) {
     for (let itemToShow of itemsToShow) {
-        document.getElementById(itemToShow).style.display = "inline-block";
+        let element = document.getElementById(itemToShow)
+        if (element) {
+            element.style.display = "inline-block"
+        }
     }
 }
 
+// Hide all elements in the array of itemsToHide
 function hideItems(itemsToHide) {
     for (let itemToHide of itemsToHide) {
-        document.getElementById(itemToHide).style.display = "none";
+        let element = document.getElementById(itemToHide)
+        if (element) {
+            element.style.display = "none"
+        }
     }
 }
+
+// Setup listener for firebase authentication state change. Hide information if the user
+// is not logged in and show certain information if they are logged in.
+firebase.auth().onAuthStateChanged(user => {
+    let itemsToHideWhenNotLoggedIn = ["userProfileMenu", "otherResourcesButton", "infographics"]
+    let itemsToHideWhenLoggedIn = ["signUpButton", "signInButton"]
+    if (user) {
+        showItems(itemsToHideWhenNotLoggedIn)
+        hideItems(itemsToHideWhenLoggedIn)
+    } else {
+        showItems(itemsToHideWhenLoggedIn)
+        hideItems(itemsToHideWhenNotLoggedIn)
+    }
+})
